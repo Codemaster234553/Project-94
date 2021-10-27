76636f678ac5603b39d6ded6e4bd746a081588a4
Codemaster234553 <84817498+Codemaster234553@users.noreply.github.com>
2021-10-27 19:31:55 -0400

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBefuMPjXNdxC0v94no0rL5R7OebBpKmpE",
+    authDomain: "project-93-80da3.firebaseapp.com",
+    projectId: "project-93-80da3",
+    storageBucket: "project-93-80da3.appspot.com",
+    messagingSenderId: "1081711699556",
+    appId: "1:1081711699556:web:093f854b2116321a3e2171",
+    measurementId: "G-MRHMZBX339"
+  };
+//ADD YOUR FIREBASE LINKS
